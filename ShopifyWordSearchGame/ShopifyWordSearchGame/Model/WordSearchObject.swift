ae8474112a30fd9013dcf2829b4ab7bf806a82ed
Graham Sela <grahamjsela@gmail.com>
2020-05-08 16:22:00 -0700

    Finished majority of layout in Main.storyboard. Need to add aesthetic changes still. Have not connected IBoutlets or done any coding yet.

---
+++ ShopifyWordSearchGame/ShopifyWordSearchGame/Model/WordSearchObject.swift
@@ -0,0 +1,13 @@
+//
+//  WordSearchObject.swift
+//  ShopifyWordSearchGame
+//
+//  Created by Graham Sela on 2020-05-08.
+//  Copyright © 2020 Graham Sela. All rights reserved.
+//
+
+import Foundation
+
+struct wordSearchObject {
+    
+}
